6c0d6d128449ccfd41c33044acee8abbdfcdeeef
Michael Hsu <michael100.hsu@gmail.com>
2018-08-23 20:50:50 -0500

    added raw database of airports in the world in json file

---
+++ manager/src/assets/airports/parse_airports.py
@@ -0,0 +1,34 @@
+import sys
+import json
+
+if len(sys.argv) != 2:
+    print "Error: Please include one argument"
+    exit(1)
+
+try:
+    file = open(sys.argv[1], "r")
+except Exception as e:
+    print "Error: File not found."
+    raise
+
+lines = [line.rstrip('\n') for line in file]
+airport_dict = {}
+for line in lines:
+    parsed_line = line.split(',')
+    airport_dict[parsed_line[0]] = {
+        "name": parsed_line[1].strip("\""),
+        "city": parsed_line[2].strip("\""),
+        "country": parsed_line[3].strip("\""),
+        "iata": parsed_line[4].strip("\""),
+        "icao": parsed_line[5].strip("\""),
+        "latitude": parsed_line[6].strip("\""),
+        "longitude": parsed_line[7].strip("\""),
+        "timezone": parsed_line[9].strip("\"")
+    }
+
+json_file = open("airports.txt", "w+")
+airport_dict = json.dumps(airport_dict)
+json_file.write(airport_dict)
+
+json_file.close()
+file.close()
